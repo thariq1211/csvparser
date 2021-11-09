32460570d889b5e9ee6fcda82de09a8ec3c99c5f
Thor <thoriqnugroho3@gmail.com>
2021-11-09 11:58:58 +0700

    initial commit

---
+++ main.js
@@ -0,0 +1,61 @@
+const parseCSV = data => {
+  // create empty array
+  const csvData = [];
+
+  // this will return each line as an individual String
+  const lines = data.split(/\n|\r/g).filter(e => e !== "");
+
+  // loop through the lines and return an array of individual
+  // Strings within the line that are separated by a comma
+
+  for (let i = 0; i < lines.length; i++) {
+    csvData[i] = lines[i].split(",");
+  }
+  // return an array of arrays 2D array
+  // e.g [ [1,2,3], [3,4,5], [6,7,8] ]
+  return csvData;
+};
+
+function download_csv_file(csvFileData) {
+  // define the heading for each row of the data
+  // let csv = "Name,Profession\n";
+  // let csv = `${csvFileData.join(",")}\n`;
+  let csv = "";
+  // merge the data with CSV
+  csvFileData.forEach(function(row) {
+    csv += row.join(",");
+    csv += "\n";
+  });
+
+  // display the created CSV data on the web browser
+  // document.write(csv);
+
+  const hiddenElement = document.createElement("a");
+  hiddenElement.href = `data:text/csv;charset=utf-8,${encodeURI(csv)}`;
+  hiddenElement.target = "_blank";
+
+  // provide the name for the CSV file to be downloaded
+  hiddenElement.download = "Famous Personalities.csv";
+  hiddenElement.click();
+}
+const fetchPerformace = async () => {
+  try {
+    const response = await fetch(
+      "https://e-verify.alpabit.com:5005/api/Download/CSV/SPVPerformanceByDate",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          supervisor_name: "spv1",
+          start_date: "2021-01-01",
+          end_date: "2021-12-31"
+        })
+      }
+    );
+    const data = await response.text();
+    const parsed = parseCSV(data);
+    download_csv_file(parsed);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
